78ac92a24b5970b97c413a29d3626d40696292ea
devlmhieu7521 <dev.lmhieu75@gmail.com>
2024-08-19 21:29:25 +0700

    update accelerometer landing to trusted

--- code/accelerometer_landing_to_trusted.py
+++ code/accelerometer_landing_to_trusted.py
@@ -4,7 +4,13 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue import DynamicFrame
 
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -21,12 +27,15 @@ AccelerometerLanding_node1724075435864 = glueContext.create_dynamic_frame.from_c
 # Script generated for node Join
 Join_node1724075479067 = Join.apply(frame1=CustomerTrusted_node1724075382481, frame2=AccelerometerLanding_node1724075435864, keys1=["email"], keys2=["user"], transformation_ctx="Join_node1724075479067")
 
-# Script generated for node Drop Fields
-DropFields_node1724075493512 = DropFields.apply(frame=Join_node1724075479067, paths=["customername", "email", "phone", "birthday", "serialnumber", "registrationdate", "lastupdatedate", "sharewithresearchasofdate", "sharewithpublicasofdate", "sharewithfriendsasofdate"], transformation_ctx="DropFields_node1724075493512")
+# Script generated for node SQL Query
+SqlQuery0 = '''
+select user, timestamp, x, y, z from myDataSource
+'''
+SQLQuery_node1723993392480 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":Join_node1724075479067}, transformation_ctx = "SQLQuery_node1723993392480")
 
 # Script generated for node Amazon S3
 AmazonS3_node1723993929869 = glueContext.getSink(path="s3://hieulm-lake-house/accelerometer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1723993929869")
 AmazonS3_node1723993929869.setCatalogInfo(catalogDatabase="stedi_project_hieulm",catalogTableName="accelerometer_trusted")
 AmazonS3_node1723993929869.setFormat("json")
-AmazonS3_node1723993929869.writeFrame(DropFields_node1723994497599)
+AmazonS3_node1723993929869.writeFrame(SQLQuery_node1723993392480)
 job.commit()
