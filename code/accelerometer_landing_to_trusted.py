80fb0f5bf8ce7fd27a9843b88feb757385bf43b8
devlmhieu7521 <dev.lmhieu75@gmail.com>
2024-08-19 20:57:02 +0700

    update accelerometer landing to trusted

--- code/accelerometer_landing_to_trusted.py
+++ code/accelerometer_landing_to_trusted.py
@@ -12,17 +12,17 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Script generated for node Accelerometer Landing
-AccelerometerLanding_node1723993612965 = glueContext.create_dynamic_frame.from_catalog(database="stedi_project_hieulm", table_name="accelerometer_landing", transformation_ctx="AccelerometerLanding_node1723993612965")
+# Script generated for node Customer Trusted
+CustomerTrusted_node1724075382481 = glueContext.create_dynamic_frame.from_catalog(database="stedi_project_hieulm", table_name="customer_trusted", transformation_ctx="CustomerTrusted_node1724075382481")
 
-# Script generated for node Customer Landing
-CustomerLanding_node1723993592216 = glueContext.create_dynamic_frame.from_catalog(database="stedi_project_hieulm", table_name="customer_landing", transformation_ctx="CustomerLanding_node1723993592216")
+# Script generated for node Accelerometer Landing
+AccelerometerLanding_node1724075435864 = glueContext.create_dynamic_frame.from_catalog(database="stedi_project_hieulm", table_name="accelerometer_landing", transformation_ctx="AccelerometerLanding_node1724075435864")
 
 # Script generated for node Join
-Join_node1723993808519 = Join.apply(frame1=AccelerometerLanding_node1723993612965, frame2=CustomerLanding_node1723993592216, keys1=["user"], keys2=["email"], transformation_ctx="Join_node1723993808519")
+Join_node1724075479067 = Join.apply(frame1=CustomerTrusted_node1724075382481, frame2=AccelerometerLanding_node1724075435864, keys1=["email"], keys2=["user"], transformation_ctx="Join_node1724075479067")
 
 # Script generated for node Drop Fields
-DropFields_node1723994497599 = DropFields.apply(frame=Join_node1723993808519, paths=["email", "phone", "birthday", "serialnumber", "registrationdate", "lastupdatedate", "sharewithresearchasofdate", "sharewithpublicasofdate", "sharewithfriendsasofdate", "customername"], transformation_ctx="DropFields_node1723994497599")
+DropFields_node1724075493512 = DropFields.apply(frame=Join_node1724075479067, paths=["customername", "email", "phone", "birthday", "serialnumber", "registrationdate", "lastupdatedate", "sharewithresearchasofdate", "sharewithpublicasofdate", "sharewithfriendsasofdate"], transformation_ctx="DropFields_node1724075493512")
 
 # Script generated for node Amazon S3
 AmazonS3_node1723993929869 = glueContext.getSink(path="s3://hieulm-lake-house/accelerometer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1723993929869")
